0a4c254367a620eb4daa71cd0e35e537e9680ab4
Aidan Younathan <ayounathan05@gmail.com>
2023-11-17 15:24:27 -0800

    Random Name Gen/storage Lecture

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+1. Add names to name pool
+2. generate a random name
+3.
+
+1.Add Name: input, button
+    take in name input and save it to a name array
+
+2. Generate a random name: button, text tag
+    Call random name function
+        Use Math.random
---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+import { RandomName } from "./randomFunction.js";
+
+let addBtn = document.getElementById("addBtn");
+let nameInput = document.getElementById("nameInput");
+let randomBtn = document.getElementById("randomBtn");
+let nameTxt = document.getElementById("nameTxt");
+let removeBtn = document.getElementById("removeBtn");
+
+let nameArray = [];
+if(localStorage.getItem("names")) {
+    nameArray = JSON.parse(localStorage.getItem("names"))
+}
+
+addBtn.addEventListener('click', function() {
+    nameArray.push(nameInput.value);
+    nameInput.value = "";
+    console.log(nameArray);
+
+    localStorage.setItem("names", JSON.stringify(nameArray))
+});
+
+randomBtn.addEventListener('click', function(){
+    nameTxt.innerText = RandomName(nameArray)
+});
+
+removeBtn.addEventListener('click', function() {
+    let index = nameArray.indexOf(nameInput.value);
+    nameArray.splice(index, 1);
+    nameInput.value = "";
+
+    localStorage.setItem("names", JSON.stringify(nameArray));
+    console.log
+});
